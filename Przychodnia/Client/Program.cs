d3b16dc82a71587e01b7594bc566641efc5ff89b
SebastianZellah <s.zellah10@gmail.com>
2022-01-20 20:17:09 +0100

    Dodano w home kalendarz

--- Przychodnia/Client/Program.cs
+++ Przychodnia/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Przychodnia.Client;
 using Syncfusion.Blazor;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,4 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
 await builder.Build().RunAsync();
